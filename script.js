9882ab3d91ce51383e339dc2c34b12bdcb68426a
Fawaz Abimbola <fawazudeenogunleye@gmail.com>
2022-11-01 02:02:28 +0100

    added a js script that resolves the navbar and responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+const showMenu = () => {
+document.querySelector('.responsive-navlinks').style.width = "100%";
+}
+
+const closeMenu = () => {
+    document.querySelector('.responsive-navlinks').style.width = "0%";
+    }
+    
