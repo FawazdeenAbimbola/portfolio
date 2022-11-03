2dfcc27e5d3e95bb14170923b668bfb4c24045c3
Fawaz Abimbola <fawazudeenogunleye@gmail.com>
2022-11-03 18:00:26 +0100

    created, styled the index page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# portfolio
+# Portfolio 1.0
 ### Hello, I am Fawaz Ogunleye. Wanna see my live portfolio? 
-[aaaa] 
+[Click here to see my portfolio](https://link-url-here.org)
--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const showMenu = () => {
-document.querySelector('.responsive-navlinks').style.width = "100%";
-}
-
-const closeMenu = () => {
-    document.querySelector('.responsive-navlinks').style.width = "0%";
+    document.querySelector('.responsive-navlinks').style.width = "100%";
     }
     
+    const closeMenu = () => {
+        document.querySelector('.responsive-navlinks').style.width = "0%";
+        }
+        
+       
+        function myLoader(){
+            var timeout = setTimeout(showPage, 4000);
+        }
+        function showPage(){
+            document.querySelector('#loader').style.display = 'none';
+            document.querySelector('.site-header').style.display = 'block';
+
+        }
+     var i = 0;
+      var text ='                 Fawaz Ogunleye';
+      var speed = 200;
+
+        function introText(){
+            if(i < text.length) {
+                document.getElementById("name").innerHTML += text.charAt(i);
+            i++;
+            }
+            setTimeout(introText, speed);
+        }
+       
+        
